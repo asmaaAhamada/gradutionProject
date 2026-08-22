9dc976644d953d3a4476c475cc35b35034d4e605
asmaaAhamada <asmaalhamada4@gmail.com>
2026-08-22 21:30:26 +0300

    finer_لاعهمي

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,9 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // رفع الحد المسموح إلى 5 ميجابايت
+      },
       manifest: {
         name: 'مؤسسة عمرها',
         short_name: 'عمرها',
