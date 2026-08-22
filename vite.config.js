c86cd0eb2cd6b9f26c6062bd86be3c7db5834f58
asmaaAhamada <asmaalhamada4@gmail.com>
2026-08-22 23:45:32 +0300

    finer_public

--- vite.config.js
+++ vite.config.js
@@ -28,21 +28,22 @@ export default defineConfig({
         background_color: '#1a2b6d',
         display: 'standalone',
         orientation: 'portrait',
+        id: '/',
         scope: '/',
         start_url: '/',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: '/pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: '/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: '/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
